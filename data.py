783032276b890d1095fa61a84ea138f93d18426e
hsiehjackson <c2hsieh@ucsd.edu>
2023-04-22 23:57:37 +0000

    add hydra configs xode

--- data.py
+++ data.py
@@ -7,10 +7,16 @@ from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 
 class DataModule(pl.LightningDataModule):
-    def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", batch_size=64):
+    def __init__(
+        self, 
+        model_name="google/bert_uncased_L-2_H-128_A-2", 
+        batch_size=64,
+        max_length=128,
+    ):
         super().__init__()
 
         self.batch_size = batch_size
+        self.max_length = max_length
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     def prepare_data(self):
@@ -19,7 +25,7 @@ class DataModule(pl.LightningDataModule):
         self.val_data = cola_dataset["validation"]
 
     def tokenize_data(self, example):
-        return self.tokenizer(example["sentence"], truncation=True, padding="max_length", max_length=128)
+        return self.tokenizer(example["sentence"], truncation=True, padding="max_length", max_length=self.max_length)
 
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
